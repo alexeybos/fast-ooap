69dc88f8118e238e8ff56848cc5fcc72319ea4af
Alexey.Bosalyga <alexey.bosalyga@nexign.com>
2026-06-08 22:04:48 +0400

    last

--- src/test/java/org/skillsmart/field/GameFieldTest.java
+++ src/test/java/org/skillsmart/field/GameFieldTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class GameFieldTest {
 
     @Test
-    void createGameField_positive_no_param_constructor() {
+    void createGameField_no_param_constructor() {
         GameField field = new GameField();
         GameFieldElement elem = field.getFieldElement(new FieldCoordinate(0, 7));
         assertEquals(GameElementType.EMPTY, elem.getElementType());
@@ -20,11 +20,10 @@ class GameFieldTest {
         assertNull(elem);
         elem = field.getFieldElement(new FieldCoordinate(8, 0));
         assertNull(elem);
-
     }
 
     @Test
-    void createGameField_positive_size_param_constructor() {
+    void createGameField_size_param_constructor() {
         GameField field = new GameField(3);
         GameFieldElement elem = field.getFieldElement(new FieldCoordinate(0, 2));
         assertEquals(GameElementType.EMPTY, elem.getElementType());
@@ -42,20 +41,21 @@ class GameFieldTest {
 
     @Test
     void createGameField_less_than_min() {
-        assertThrows(IllegalArgumentException.class, () -> new FieldElementCollection(2));
+        assertThrows(IllegalArgumentException.class, () -> new GameField(2));
     }
 
     @Test
     void createGameField_grater_than_max() {
-        assertThrows(IllegalArgumentException.class, () -> new FieldElementCollection(10));
+        assertThrows(IllegalArgumentException.class, () -> new GameField(10));
     }
 
     @Test
-    void fill() {
-        GameField field = new GameField(4);
+    void fill_max() {
+        int size = 8;
+        GameField field = new GameField(size);
         field.fill();
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 assertNotEquals(GameElementType.EMPTY,field.getFieldElement(new FieldCoordinate(j, i)).getElementType(),
                         "Ячейка (" + j + "," + i + ") не должна быть пустой");
             }
@@ -63,11 +63,21 @@ class GameFieldTest {
     }
 
     @Test
-    void createCoordinateFromUserInput() {
+    void fill() {
+        int size = 4;
+        GameField field = new GameField(size);
+        field.fill();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                assertNotEquals(GameElementType.EMPTY,field.getFieldElement(new FieldCoordinate(j, i)).getElementType(),
+                        "Ячейка (" + j + "," + i + ") не должна быть пустой");
+            }
+        }
+    }
+
+    @Test
+    void createCoordinateFromUserInput_exists() {
         GameField field = new GameField(3);
-        assertNull(field.createCoordinateFromUserInput("a12"));
-        assertNull(field.createCoordinateFromUserInput("a4"));
-        assertNull(field.createCoordinateFromUserInput("d1"));
         FieldCoordinate coordinate = field.createCoordinateFromUserInput("a3");
         assertEquals(0, coordinate.getX());
         assertEquals(2, coordinate.getY());
@@ -77,6 +87,11 @@ class GameFieldTest {
     }
 
     @Test
-    void show() {
+    void createCoordinateFromUserInput_not_exists() {
+        GameField field = new GameField(3);
+        assertNull(field.createCoordinateFromUserInput("a12"));
+        assertNull(field.createCoordinateFromUserInput("a4"));
+        assertNull(field.createCoordinateFromUserInput("d1"));
     }
+
 }
